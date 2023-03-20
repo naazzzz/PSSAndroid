29ec89d138cd2a1087869083678a8c85f6417c14
artem <artem_berezhnov@mail.ru>
2023-03-21 01:10:02 +0300

    добавил использование web api обращаюсь по http к Pet search service

--- app/src/main/java/com/example/myapplication/authActivity.java
+++ app/src/main/java/com/example/myapplication/authActivity.java
@@ -1,12 +1,19 @@
 package com.example.myapplication;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.JsonReader;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,8 +21,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.orm.SugarRecord;
 import com.yandex.mapkit.MapKitFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 import java.util.Objects;
+
+import javax.net.ssl.HttpsURLConnection;
 
 
 public class authActivity extends AppCompatActivity {
@@ -36,6 +51,7 @@ public class authActivity extends AppCompatActivity {
     }
 
     public void transitionHome(View view) {
+
         EditText username_text= findViewById(R.id.editText2);
         String username = username_text.getText().toString();
 
@@ -43,31 +59,123 @@ public class authActivity extends AppCompatActivity {
         EditText password_text= findViewById(R.id.editText);
         String password = password_text.getText().toString();
 
-        List<User> users= SugarRecord.find(User.class,"username=?",username);
+        //List<User> users= SugarRecord.find(User.class,"username=?",username);
         Context context = getApplicationContext();
-        if (!users.isEmpty()){
-        for(int i=0;i< users.size();i++) {
-            User user = users.get(i);
-            if (Objects.equals(user.pass, password)) {
-                Toast toast = Toast.makeText(context, "Успешная авторизация",
-                        Toast.LENGTH_LONG);
-                toast.show();
-                Intent intent = new Intent(this, MainActivity.class);
-                startActivity(intent);
-            } else {
-                Toast toast = Toast.makeText(context, "Проверьте корректность введенных данных",
-                        Toast.LENGTH_LONG);
-                toast.show();
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                // Create URL
+                URL githubEndpoint = null;
+
+                try {
+                    githubEndpoint = new URL("http://10.0.2.2:8080/user/"+username);
+                } catch (MalformedURLException e) {
+                    throw new RuntimeException(e);
+                }
+                    // Create connection
+                try {
+                    HttpURLConnection myConnection =
+                            (HttpURLConnection) githubEndpoint.openConnection();
+                    myConnection.setRequestProperty("User-Agent", "my-rest-app-v0.1");
+                    myConnection.setRequestMethod("GET");
+                    if (myConnection.getResponseCode() == 200) {
+                        // Success
+                        // Further processing here
+                        //Чтение тела ответа
+                        InputStream responseBody = myConnection.getInputStream();
+                        InputStreamReader responseBodyReader = new InputStreamReader(responseBody, "UTF-8");
+                        JsonReader jsonReader = new JsonReader(responseBodyReader);
+                        jsonReader.beginObject(); // Start processing the JSON object
+                        boolean name=false;
+                        boolean pass=false;
+                        while (jsonReader.hasNext()) { // Loop through all keys
+                            String key = jsonReader.nextName(); // Fetch the next key
+                            if (key.equals("username") || key.equals("pass")) { // Check if desired key
+                                // Fetch the value as a String
+                                String value1 = jsonReader.nextString();
+                                System.out.println(value1);
+                                if(value1.equals(username)){
+                                    name=true;
+                                }
+                                if(value1.equals(password)){
+                                    pass=true;
+                                }
+                                 // Break out of the loop
+                            } else {
+                                jsonReader.skipValue(); // Skip values of other keys
+                            }
+
+                        }
+
+                        myConnection.disconnect();
+                        if(name && pass){
+                            authActivity.this.runOnUiThread(new Runnable() {
+                                public void run() {
+                                    Toast toast = Toast.makeText(context, "Успешная авторизация", Toast.LENGTH_LONG);
+                                    toast.show();
+                                }
+                            });
+                            Intent intent = new Intent(authActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            authActivity.this.runOnUiThread(new Runnable() {
+                                public void run() {
+                                    Toast toast = Toast.makeText(context, "Проверьте корректность введенных данных", Toast.LENGTH_LONG);
+                                    toast.show();
+                                }
+                            });
+                        }
+
+
+                    } else {
+                        authActivity.this.runOnUiThread(new Runnable() {
+                            public void run() {
+                                Toast toast = Toast.makeText(context, "Проверьте корректность введенных данных", Toast.LENGTH_LONG);
+                                toast.show();
+                            }
+                        });
+
+                    }
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
             }
-        }
-        }
-        else{
-            Toast toast = Toast.makeText(context, "Проверьте корректность введенных данных",
-                    Toast.LENGTH_LONG);
-            toast.show();
-        }
+
+
+        });
+
+
+
+//
+//        Intent intent = new Intent(this, MainActivity.class);
+//        startActivity(intent);
+
+
+//        if (!users.isEmpty()){
+//        for(int i=0;i< users.size();i++) {
+//            User user = users.get(i);
+//            if (Objects.equals(user.pass, password)) {
+//                Toast toast = Toast.makeText(context, "Успешная авторизация",
+//                        Toast.LENGTH_LONG);
+//                toast.show();
+//                Intent intent = new Intent(this, MainActivity.class);
+//                startActivity(intent);
+//            } else {
+//
+//            }
+//        }
+//        }
+//        else{
+//            Toast toast = Toast.makeText(context, "Проверьте корректность введенных данных",
+//                    Toast.LENGTH_LONG);
+//            toast.show();
+//        }
 
 
 
     }
+
 }
